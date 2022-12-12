974d3153708d923860d311b210a49e3200e20c11
Xyyyy04 <13261054122@163.com>
2022-12-12 20:47:22 +0800

    20301129 冯馨瑶

---
+++ user/target/riscv64gc-unknown-none-elf/release/deps/03power_6-3dd00536c069213e.d
@@ -0,0 +1,5 @@
+/mnt/user/target/riscv64gc-unknown-none-elf/release/deps/03power_6-3dd00536c069213e: src/bin/03power_6.rs
+
+/mnt/user/target/riscv64gc-unknown-none-elf/release/deps/03power_6-3dd00536c069213e.d: src/bin/03power_6.rs
+
+src/bin/03power_6.rs:
---
+++ user/target/riscv64gc-unknown-none-elf/release/deps/04sleep-b56debf22eaacdcf.d
@@ -0,0 +1,5 @@
+/mnt/user/target/riscv64gc-unknown-none-elf/release/deps/04sleep-b56debf22eaacdcf: src/bin/04sleep.rs
+
+/mnt/user/target/riscv64gc-unknown-none-elf/release/deps/04sleep-b56debf22eaacdcf.d: src/bin/04sleep.rs
+
+src/bin/04sleep.rs:
