1931d1f1308e08870edc4d15c1c6221d6d48ee42
Xyyyy04 <13261054122@163.com>
2022-12-18 22:35:10 +0800

    20301129 冯馨瑶

--- os/src/task/mod.rs
+++ os/src/task/mod.rs
@@ -24,6 +24,24 @@ pub use processor::{
 pub use manager::add_task;
 pub use pid::{PidHandle, pid_alloc, KernelStack};
 
+pub fn suspend_current_and_run_next() {
+    // There must be an application running.
+    let task = take_current_task().unwrap();
+
+    // ---- access current TCB exclusively
+    let mut task_inner = task.inner_exclusive_access();
+    let task_cx_ptr = &mut task_inner.task_cx as *mut TaskContext;
+    // Change status to Ready
+    task_inner.task_status = TaskStatus::Ready;
+    drop(task_inner);
+    // ---- release current PCB
+
+    // push back to ready queue.
+    add_task(task);
+    // jump to scheduling cycle
+    schedule(task_cx_ptr);
+}
+
 lazy_static! {
     pub static ref INITPROC: Arc<TaskControlBlock> = Arc::new(
         TaskControlBlock::new(get_app_data_by_name("initproc").unwrap())
